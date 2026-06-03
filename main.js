1426e87754648c4cd74ef9c93035fd871b5a9e2a
AMATULLAH <ame>
2026-06-04 02:10:01 +0300

    update code for mobile responsive

--- main.js
+++ main.js
@@ -1,19 +1,25 @@
 console.log("JavaScript connected!");
 
-
+// hamburger and close icon
 const navbar = document.getElementById("navbar");
 const menuToggle = document.querySelector(".menu-toggle");
 const closeBtn = document.querySelector(".close-btn");
 const overlay = document.querySelector(".overlay");
 
-menuToggle.onclick = () => {
-    navbar.classList.add("active");
-};
+if (menuToggle) {
+    menuToggle.onclick = () => {
+        navbar.classList.add("active");
+    };
+}
 
-closeBtn.onclick = () => {
-    navbar.classList.remove("active");
-};
+if (closeBtn) {
+    closeBtn.onclick = () => {
+        navbar.classList.remove("active");
+    };
+}
 
-overlay.onclick = () => {
-    navbar.classList.remove("active");
-};
+if (overlay) {
+    overlay.onclick = () => {
+        navbar.classList.remove("active");
+    };
+}
